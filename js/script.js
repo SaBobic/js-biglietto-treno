95bbb17823d175ca3bd7dcf7f7d708b47f1230aa
SaBobic <fabrizioettori@gmail.com>
2022-06-21 15:27:02 +0200

    added working script without front-end

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/*
+1- Chiedo quanti km deve percorrere
+2- Chiedo quanti anni ha
+3- Dichiaro la variabile age per la tariffa piena
+4- Modifico la variabile age per i minorenni
+5- Modifico la variabile age per gli over 65
+6- Mostro il prezzo finale, esplicitando anche della riduzione ottenuta
+*/
+
+// 1- Chiedo quanti km deve percorrere
+
+const km = parseInt(prompt('Quanti chilometri devi percorrere?'));
+console.log("L'utente deve percorrere " + km + " chilometri");
+
+// 2- Chiedo quanti anni ha
+
+const age = parseInt(prompt('Quanti anni hai?'));
+console.log("L'utente ha " + age + " anni");
+
+// 3- Dichiaro la variabile age per la tariffa piena
+
+let price = km*0.21;
+console.log('La tariffa piena è di ' + price + "€");
+
+// 4- Modifico la variabile age per i minorenni
+if (age < 18){
+    price = (price / 100) * 80;
+}
+
+// 5- Modifico la variabile age per gli over 65
+
+if (age >= 65){
+    price = (price / 100) * 60;
+}
+
+console.log(price);
