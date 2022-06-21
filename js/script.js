6da621ad4f362be5b1d1845f5922e208196b4e3d
SaBobic <fabrizioettori@gmail.com>
2022-06-21 17:16:01 +0200

    added input validation

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 /*
-1- Chiedo quanti km deve percorrere
-2- Chiedo quanti anni ha
-3- Dichiaro la variabile age per la tariffa piena
-4- Modifico la variabile age per i minorenni
-5- Modifico la variabile age per gli over 65
-6- Limito a 2 le cifre decimali di price
-7- Mostro il prezzo finale, esplicitando anche della riduzione ottenuta
+1. Chiedo quanti km deve percorrere
+2. Chiedo quanti anni ha
+3. Validazione dei dati indicati dall'utente
+4. Dichiaro la variabile price per la tariffa piena
+5. Dichiaro la variabile newPrice per minorenni e over 65
+6. Limito a 2 le cifre decimali di price e newPrice
+7. Mostro dati inseriti e prezzo finale su schermo, esplicitando anche la riduzione ottenuta
 */
 
-// 1- Chiedo quanti km deve percorrere
+// 1. Chiedo quanti km deve percorrere
 
 const km = parseInt(prompt('Quanti chilometri devi percorrere?', '125'));
 
-// 2- Chiedo quanti anni ha
+// 2. Chiedo quanti anni ha
 
-const age = parseInt(prompt('Quanti anni hai?'));
+const age = parseInt(prompt('Quanti anni hai?', '20'));
 
-// 3- Dichiaro la variabile age per la tariffa piena
+// 3. Validazione dei dati indicati dall'utente
+
+if (km < 1 || isNaN(km)){
+    alert("Chilometri non inseriti correttamente");
+}
+
+if (age < 0 || isNaN(age)){
+    alert("Anni non inseriti correttamente");
+}
+
+// 4. Dichiaro la variabile price per la tariffa piena
 
 let price = km*0.21;
 
-// 4- Modifico la variabile age per i minorenni
+// 5. Dichiaro la variabile newPrice per minorenni e over 65
 
 let newPrice;
 
@@ -28,20 +38,19 @@ if (age < 18){
     newPrice = (price / 100) * 80;
 }
 
-// 5- Modifico la variabile age per gli over 65
-
 if (age >= 65){
     newPrice = (price / 100) * 60;
 }
 
-// 6- Limito a 2 le cifre decimali di price
+// 6. Limito a 2 le cifre decimali di price e newPrice
+
+price = price.toFixed(2);
+
 if (!isNaN(newPrice)){
     newPrice = newPrice.toFixed(2);
 }
 
-price = price.toFixed(2);
-
-// 7- Mostro il prezzo finale, esplicitando anche della riduzione ottenuta
+// 7. Mostro dati inseriti e prezzo finale su schermo, esplicitando anche la riduzione ottenuta
 
 document.getElementById('km').innerText = km + " km";
 
@@ -53,4 +62,3 @@ if ((age < 18) || (age >= 65)){
 } else {
     document.getElementById('price').innerText = price + "€";
 }
-
